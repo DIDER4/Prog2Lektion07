1a02496ea5b17a17adb2c5dc04bbb50984dd25fb
Benn Christensen <benn.christensen@wolterskluwer.com>
2025-02-15 22:39:12 +0100

    added starter of exercise 01

---
+++ src/opgave01/Main.java
@@ -0,0 +1,15 @@
+package opgave01;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+        ArrayList<Integer> integers = new ArrayList<>(List.of(32, 23, 13, -22, 202, 42, 53));
+        System.out.println("Antal lige tal = " + NumberOfEvenNumbers(integers));
+    }
+
+    private static int NumberOfEvenNumbers(ArrayList<Integer> integers) {
+        return 0;
+    }
+}
