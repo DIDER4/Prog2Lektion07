9a40b53d128a2e3e5ce7fb04c3cdd4464d0a3f95
Benn Christensen <benn.christensen@wolterskluwer.com>
2025-02-16 14:26:32 +0100

    renamed numberOfEvenNumbers method

--- src/opgave01/Main.java
+++ src/opgave01/Main.java
@@ -5,11 +5,11 @@ import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-        ArrayList<Integer> integers = new ArrayList<>(List.of(32, 23, 13, -22, 202, 42, 53));
-        System.out.println("Antal lige tal = " + NumberOfEvenNumbers(integers));
+        ArrayList<Integer> integers = new ArrayList<>(List.of(32, 23, 13, -22, 202, 42, 53, 0));
+        System.out.println("Antal lige tal = " + numberOfEvenNumbers(integers));
     }
 
-    private static int NumberOfEvenNumbers(ArrayList<Integer> integers) {
+    private static int numberOfEvenNumbers(ArrayList<Integer> integers) {
         return 0;
     }
 }
