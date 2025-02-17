211ef2066b99ca36e3c89d816343471f12475f9d
Benn Christensen <benn.christensen@wolterskluwer.com>
2025-02-17 20:10:17 +0100

    added exercise 5

--- src/opgave03/Main.java
+++ src/opgave03/Main.java
@@ -8,17 +8,7 @@ public class Main {
     }
 
     private static int threeXPlusOne(int x) {
-        int count = 0;
-        while (x > 1) {
-            if (x % 2 == 0) {
-                x = x / 2;
-            }
-            else {
-                x = 3 * x + 1;
-            }
-            count++;
-        }
-        return count;
+        return 0;
     }
 
 }
